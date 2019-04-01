4a73c4fc6517d0c5835f6c10865fdb926425fb9a
Claudio Cicimurri <claudio.ccmrr@gmail.com>
2019-04-01 17:58:34 +0200

    Aggiunta Nazionalità Spostati gli id avanti di 1

--- estrai.js
+++ estrai.js
@@ -46,15 +46,18 @@ console.timeEnd(" > " + bold("tutti"));
 // ANCHOR Inserisci nell'SQL i valori estratti
 console.log(bold("-- SCRITTURA SU FILE --"));
 scriviTabella("Generi", "idGenere, Descrizione", generi,
-    (i, val) => `${i}, '${val}'`);
+    (i, val) => `${i + 1}, '${val}'`);
 scriviTabella("Tipologie", "idTipologia, Descrizione", tipologie,
-    (i, val) => `${i}, '${val}'`);
+    (i, val) => `${i + 1}, '${val}'`);
 scriviTabella("Lingue", "idLingua, Descrizione, Abbreviazione", lingue,
-    (i, val) => `${i}, '${val}', '${val.replace('/', '').substring(0, 2).toUpperCase()}'`);
+    (i, val) => `${i + 1}, '${val}', '${val.replace('/', '').substring(0, 2).toUpperCase()}'`);
 scriviTabella("Editori", "idEditore, Nome, Descrizione", editori,
-    (i, val) => `${i}, "${val}", NULL`);
+    (i, val) => `${i + 1}, "${val}", NULL`);
 scriviTabella("Collane", "idCollana, Nome", collane,
-    (i, val) => `${i}, "${val}"`);
+    (i, val) => `${i + 1}, "${val}"`);
+scriviTabella("Nazionalita", "idNazionalita, Descrizione",
+    ["N/D", "italiana", "inglese", "polacca"],
+    (i, val) => `${i + 1}, "${val}"`);
 scriviTabella("Autori", "idAutore, NomeAutore, CognomeAutore, DataNascita, idNazionalita, idCittaNascita", autori,
     (i, val) => {
         const { nome, cognome } = sistemaNomi(val);
